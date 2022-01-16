6477e9854321437493ed2fc63bbe4b8b5e655e3d
leehyeonseok <leehyeonseok@218.38.137.27>
2022-01-16 23:06:30 +0900

    타일즈 사용하기 전까지 했

---
+++ src/main/java/com/myboot03/Myboot03Application.java
@@ -0,0 +1,14 @@
+package com.myboot03;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myboot03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myboot03Application.class, args);
+	}
+
+}
